4af1e95a89098600cc70f33174b87dc538050d78
Abhay Singh T <114575759+Abhay7860t@users.noreply.github.com>
2024-03-01 19:30:23 +0530

    commit

--- index.js
+++ index.js
@@ -16,11 +16,7 @@ function restCall() {
             // var data = this.response;
 
             document.getElementById("response").innerHTML = this.status + this.response;
-            // var name = document.getElementById('name').value = data;
-            // var email = document.getElementById('email').value = data;
-            // var phone = document.getElementById('phone').value = data;
-            // var password = document.getElementById('phone').value = data;
-            // data = JSON.parse(data);
+
         }
     };
     client.send(requestBody);
