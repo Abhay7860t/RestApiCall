a086885cce711e0bb890fd0d0107a6b80596ad5d
Abhay Singh T <114575759+Abhay7860t@users.noreply.github.com>
2024-03-01 12:00:05 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function restCall() {
-    var requestBody = "{\"u_name\":\"AbhaySingh\",\"u_email\":\"abhay@gmail.com\",\"u_phone_number\":\"7678987890\",\"u_password\":\"Abhay786$%\"}";
+    var requestBody = "{\"u_name\":\"AbhaySingh1\",\"u_email\":\"test@gmail.com\",\"u_phone_number\":\"7678985678\",\"u_password\":\"Abha@123\"}";
 
     var client = new XMLHttpRequest();
     client.open("post", "https://dev75213.service-now.com/api/now/table/u_userrecords");
 
     client.setRequestHeader('Accept', 'application/json');
     client.setRequestHeader('Content-Type', 'application/json');
-   // client.setRequestHeader('Access-Control-Allow-Origin', 'https://abhay7860t.github.io/RestApiCall/');
+    client.setRequestHeader('Access-Control-Allow-Origin', 'https://abhay7860t.github.io/RestApiCall/');
 
     //Eg. UserName="admin", Password="admin" for this code sample.
     client.setRequestHeader('Authorization', 'Basic ' + btoa('AbhaySingh' + ':' + 'Abha@123'));
@@ -16,7 +16,7 @@ function restCall() {
             // var data = this.response;
 
             document.getElementById("response").innerHTML = this.status + this.response;
-           // console.log(this.status + this.response, "Response")
+            console.log(this.status + this.response, "Response")
             // var name = document.getElementById('name').value = data;
             // var email = document.getElementById('email').value = data;
             // var phone = document.getElementById('phone').value = data;
@@ -26,6 +26,7 @@ function restCall() {
     };
     client.send(requestBody);
 }
+
 
 
 
