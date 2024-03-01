35427c557febc795ea25b982ba845c9ec8be2c3c
Abhay Singh T <114575759+Abhay7860t@users.noreply.github.com>
2024-03-01 22:09:18 +0530

    commi

--- index.js
+++ index.js
@@ -1,26 +1,44 @@
-function getToken(url, clientID, clientSecret, callback) {
-    var token_; // variable will store the token
-    var userName = "d3bde483f964821008c882ecd5e25efa"; // app clientID
-    var passWord = "?N!Gf3(7WB"; // app clientSecret
-    // var caspioTokenUrl = "https://dev75213.service-now.com/oauth_token.do"; // Your application token endpoint  
-    var request = new XMLHttpRequest();
-    request.open("POST","https://dev75213.service-now.com/oauth_token.do");
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.setRequestHeader('Access-Control-Allow-Origin', 'https://abhay7860t.github.io/RestApiCall/');
-    request.onreadystatechange = function () {
-        if (request.readyState == XMLHttpRequest.DONE) {
-            if (request.status == 200) {
-                var response = JSON.parse(request.responseText);
-                var key = response.access_token;
-                token_ = key;
-                callback(null, key); // Call the callback with the token
+var clientId = 'd3bde483f964821008c882ecd5e25efa';
+var clientSecret = '?N!Gf3(7WB';
+var tokenUrl = 'https://dev75213.service-now.com/oauth_token.do'; // Token endpoint provided by your OAuth 2.0 provider
+
+function generateToken(clientId, clientSecret, tokenUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', tokenUrl, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                var response = JSON.parse(xhr.responseText);
+                var accessToken = response.access_token;
+                console.log('Access Token:', accessToken);
+                // Handle the access token
             } else {
-                callback("Error getting token", null);
+                console.error('Error:', xhr.status);
+                // Handle the error
             }
         }
     };
-    request.send("grant_type=refresh_token&client_id=" + encodeURIComponent(userName) + "&client_secret=" + encodeURIComponent(passWord));
+
+    var requestBody = 'grant_type=client_credentials' +
+        '&client_id=' + encodeURIComponent(clientId) +
+        '&client_secret=' + encodeURIComponent(clientSecret);
+
+    xhr.send(requestBody);
 }
+
+// Call the function to generate the token
+generateToken(clientId, clientSecret, tokenUrl);
+
+
+
+
+
+
+
+
+
 
 
 
