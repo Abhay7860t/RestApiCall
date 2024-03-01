725fc858cb29c0ac6fbf49510eec198ad71719fb
Abhay Singh T <114575759+Abhay7860t@users.noreply.github.com>
2024-03-01 09:47:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function restCall() {
+    var requestBody = "{\"u_name\":\"test\",\"u_email\":\"test@gmail.com\",\"u_phone_number\":\"7678985678\",\"u_password\":\"Abha@123\"}";
+
+    var client = new XMLHttpRequest();
+    client.open("post", "https://dev75213.service-now.com/api/now/table/u_userrecords");
+
+    client.setRequestHeader('Accept', 'application/json');
+    client.setRequestHeader('Content-Type', 'application/json');
+    client.setRequestHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500/');
+
+    //Eg. UserName="admin", Password="admin" for this code sample.
+    client.setRequestHeader('Authorization', 'Basic ' + btoa('AbhaySingh' + ':' + 'Abha@123'));
+
+    client.onreadystatechange = function () {
+        if (this.readyState == this.DONE) {
+            var data = this.response;
+
+            document.getElementById("response").innerHTML = this.status + this.response;
+            console.log(this.status + this.response, "Response")
+            document.getElementById("response").innerHTML = this.status + this.response;
+            var name = document.getElementById('name').value = data;
+            var email = document.getElementById('email').value = data;
+            var phone = document.getElementById('phone').value = data;
+            var password = document.getElementById('phone').value = data;
+            data = JSON.parse(data);
+        }
+    };
+    client.send(requestBody);
+}
+
+
+
